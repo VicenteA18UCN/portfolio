3340b8e085def6673e5bc30324a51b787496ecf2
VicenteA18UCN <vicente.alarcon@alumnos.ucn.cl>
2026-02-18 18:33:58 -0300

    feat: add layout, header, footer and hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import robotsTxt from "astro-robots-txt";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [ robotsTxt()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
