daa3ed13153cd348dc794f2db68b98d5502f8816
VicenteA18UCN <vicente.alarcon@alumnos.ucn.cl>
2026-02-18 20:11:50 -0300

    feat: add AboutMe, Experience, and Projects components; integrate icons and update index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import robotsTxt from "astro-robots-txt";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [ robotsTxt()],
+  integrations: [robotsTxt(), icon()],
 
   vite: {
     plugins: [tailwindcss()],
