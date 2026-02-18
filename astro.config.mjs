84509aafed50fbfe6c597b8dd88d737135c293b8
VicenteA18UCN <vicente.alarcon@alumnos.ucn.cl>
2026-02-18 20:45:49 -0300

    fix: update site configuration to use dynamic base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vicentea18ucn.github.io/portfolio/",
+  base: process.env.NODE_ENV === "production" ? "/portfolio" : "/",
   integrations: [robotsTxt(), icon()],
   vite: {
     plugins: [tailwindcss()],
